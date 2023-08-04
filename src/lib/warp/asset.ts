b355caacd289f465492487bed01124d3c4903621
Pawan Paudel <pawanpaudel93@gmail.com>
2023-08-04 16:08:21 +0545

    fix: type ignore

--- src/lib/warp/asset.ts
+++ src/lib/warp/asset.ts
@@ -1,4 +1,6 @@
+// @ts-ignore
 import { WarpFactory, LoggerFactory } from "warp-contracts";
+// @ts-ignore
 import { DeployPlugin } from "warp-contracts-plugin-deploy";
 
 LoggerFactory.INST.logLevel("error");

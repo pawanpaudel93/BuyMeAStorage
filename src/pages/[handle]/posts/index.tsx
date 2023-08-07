f422efcb17baf6d7db25e278ce88c01b44172731
Mikma Tamang <mikdorje@gmail.com>
2023-08-08 02:20:58 +0545

    padding add

--- src/pages/[handle]/posts/index.tsx
+++ src/pages/[handle]/posts/index.tsx
@@ -264,7 +264,7 @@ export default function SupportPage({ address }: { address?: string }) {
             defaultActiveKey="2"
             items={items}
             onChange={onTabChange}
-            style={{ padding: "4px 24px" }}
+            style={{ padding: "14px 24px" }}
             tabBarGutter={12}
           />
         </div>
